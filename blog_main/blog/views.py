886cc222f2d30fe4484441aa2c31ce7a18bae005
Walloonam <jh37106@gmail.com>
2022-03-18 16:10:28 +0900

    블로그 목로 뷰

--- blog_main/blog/views.py
+++ blog_main/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
+
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+
+    )
