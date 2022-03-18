023cc4c34944b3004eece638c625f76f3d63f707
Walloonam <jh37106@gmail.com>
2022-03-18 17:14:57 +0900

    블로그 상세 페이지 작성2

--- blog_main/blog/views.py
+++ blog_main/blog/views.py
@@ -1,28 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
 from .models import Post
 
-def single_post_page(request,pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
 
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-
-    )
+class PostDetail(DetailView):
+    model = Post
--- blog_main/single_pages/views.py
+++ blog_main/single_pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def about_me(request):
+    return render(
+        request,
+        'single_pages/about_me.html'
+    )
+
+
+def landing(request):
+    return render(
+        request,
+        'single_pages/landing.html'
+    )
