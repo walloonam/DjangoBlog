507946d1bc77c5a5991b2a810742f16982bc3285
Walloonam <jh37106@gmail.com>
2022-04-08 15:34:12 +0900

    post에 author필드 추가

--- blog_main/blog/models.py
+++ blog_main/blog/models.py
@@ -1,9 +1,17 @@
 import os.path
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title=models.CharField(max_length=30)
@@ -17,9 +25,12 @@ class Post(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
 
+    author = models.ForeignKey(User, null=True, blank=True ,on_delete=models.CASCADE)
+
+
 
     def __str__(self):
-        return f'({self.pk}){self.title}'
+        return f'({self.pk}){self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
