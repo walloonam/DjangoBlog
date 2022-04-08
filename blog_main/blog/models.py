e47ae04daa9a5933baa39b0c1db3c95b691c32f3
Walloonam <jh37106@gmail.com>
2022-04-08 13:08:32 +0900

    hook메시지 작성

--- blog_main/blog/models.py
+++ blog_main/blog/models.py
@@ -9,6 +9,8 @@ class Post(models.Model):
     title=models.CharField(max_length=30)
     content=models.TextField()
 
+    hook_msg = models.TextField(blank=True)
+
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
